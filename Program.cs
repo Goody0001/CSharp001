9c59ed64018d7f31817c8bec8ffc60449aac66bc
Goody001 <oghalegoodness@gmail.com>
2020-05-02 13:37:50 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnFromHome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
